152d8b64bee83af5c796cc43a4d9096ac6e6b21d
Tanya Kotan <tanyakotan@Tanyas-MBP.attlocal.net>
2017-07-20 15:35:45 -0400

    first draft of js/jquery pseudocode done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,57 @@
-console.log("What's up?");
 $(() => {
 	$
   console.log("What's up?");
 });
+// Main game
+// 1. The player accesses the home page.
+      // - all scores must be set to zero as soon as a player lands on this page.
+// 2. He makes a selection based on the picture.
+// 3. It takes him to another page.
+// 4. The page says computer is thinking.
+// 5. Computer's thinking is represented by a question mark
+// 6. that rocks back and forth for 3-4 cycles -
+// 7. then she has an idea (lightbulb comes up).
+// 8. The button to continue pops up.
+// 9. The player selects continue.
+// 10. The player is taken to the game page.
+// 11. The player and computer choices slide into the screen.
+        // Rules of the game
+        //   Scissors cuts Paper
+        //   Paper covers Rock
+        //   Rock crushes Lizard
+        //   Lizard poisons Spock
+        //   Spock smashes Scissors
+        //   Scissors decapitates Lizard
+        //   Lizard eats Paper
+        //   Paper disproves Spock
+        //   Spock vaporizes Rock
+        //   Rock crushes Scissors
+
+// 12. The winner's object remains visible
+// 13. while the loser's object disappears.
+// 14. The scores slide up on each side of the screen. Round winner is announced.
+// 15. Buttons pop up for player to play another round or quit.
+
+//         Path 1
+//         1. If player plays another round, go to round-intro page.
+//         2. Player makes a selection based on the picture.
+//         3. Repeat steps 3-15 in main game.
+//
+//         Path 2
+//         1. If player quits, go back to home page.
+//
+// 16. There are three rounds in the game.
+// 17. After the third round, a final results button appears.
+// 18. Player goes to another page.
+//
+//         Player wins
+//         1. Player wins screen pops up.
+//         2. If player chooses to play again, he is sent to the home page.
+// 
+//         Computer wins
+//         1. Computer wins screen pops up.
+//         2. If player chooses to play again, he is sent to the home page.
+//
+//         Tie game
+//         1. Tie game screen pops up.
+//         2. If player chooses to play again, he is sent to the home page.
