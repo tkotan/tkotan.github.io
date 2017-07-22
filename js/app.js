d46a473706236289ff20630ed148395e785e8ab1
Tanya Kotan <tanyakotan@Tanyas-MBP.attlocal.net>
2017-07-22 10:53:58 -0400

    put some other functions together/cleaning up code

--- js/app.js
+++ js/app.js
@@ -1,60 +1,5 @@
 $(() => {
 
-
-// // Main game
-// // 1. The player accesses the home page.
-//       // - all scores must be set to zero as soon as a player lands on this page. const score = 0;
-// // 2. He makes a selection based on the picture. event handler
-// // 3. It takes him to another page.
-// // 4. The page says computer is thinking.
-// // 5. Computer's thinking is represented by a question mark
-// // 6. that rocks back and forth for 3-4 cycles -
-// // 7. then she has an idea (lightbulb comes up).
-// // 8. The button to continue pops up.
-// // 9. The player selects continue.
-// // 10. The player is taken to the game page.
-// // 11. The player and computer choices slide into the screen.
-//         // Rules of the game
-//         //   Scissors cuts Paper
-//         //   Paper covers Rock
-//         //   Rock crushes Lizard
-//         //   Lizard poisons Spock
-//         //   Spock smashes Scissors
-//         //   Scissors decapitates Lizard
-//         //   Lizard eats Paper
-//         //   Paper disproves Spock
-//         //   Spock vaporizes Rock
-//         //   Rock crushes Scissors
-//
-// // 12. The winner's object remains visible
-// // 13. while the loser's object disappears.
-// // 14. The scores slide up on each side of the screen. Round winner is announced.
-// // 15. Buttons pop up for player to play another round or quit.
-//
-// //         Path 1
-// //         1. If player plays another round, go to round-intro page.
-// //         2. Player makes a selection based on the picture.
-// //         3. Repeat steps 3-15 in main game.
-// //
-// //         Path 2
-// //         1. If player quits, go back to home page.
-// //
-// // 16. There are three rounds in the game.
-// // 17. After the third round, a final results button appears.
-// // 18. Player goes to another page.
-// //
-// //         Player wins
-// //         1. Player wins screen pops up.
-// //         2. If player chooses to play again, he is sent to the home page.
-// //
-// //         Computer wins
-// //         1. Computer wins screen pops up.
-// //         2. If player chooses to play again, he is sent to the home page.
-// //
-// //         Tie game
-// //         1. Tie game screen pops up.
-// //         2. If player chooses to play again, he is sent to the home page.
-//
 //variables
 const rpsls = ["rock","paper","scissors","lizard","spock"]; //game choices
 let round = 1; //round number
@@ -62,24 +7,28 @@ let computerScore = 0;
 let finalComputerScore = computerScore;
 let playerScore = 0;
 let finalPlayerScore = playerScore; //the final player score is the sum of the player scores.
-
-let computerHand = "paper";
-let playerHand = "scissors"; //the player's choice
+let computerHand;
+let playerHand; //the player's choice
 let computerWinsRound = false;
 let playerWinsRound = false;
 let computerWinsGame = false;
 let playerWinsGame = false; //no one has won or lost yet
 let roundTie = false;
 let gameTie = false; //no one has a tie game yet
+//
+// setTimeout (() => {
+//    confirm("Candie the Computer has made her choice.  Are you ready for the results?");
+// }, 3000);
 
-// //
-// // //objects
-// //
-// //
-// //
-//
-//
-// //functions
+// setTimeout(() => {
+//   ($('#computer-message').dialog());
+// }, 3000);
+
+// const computerThinks = $(e.currentTarget).on('click',(e) => {
+//         $('computer-message').dialog();
+//     });
+
+
   const reset = () => {
     round = 1; //round number
     computerScore = 0;
@@ -90,17 +39,24 @@ let gameTie = false; //no one has a tie game yet
     playerWinsGame = false; //no one has won or lost yet
     roundTie = false;
     gameTie = false; //no one has a tie game yet
-    // playerWinsRound("scissors", "paper");
   }
 
 
-//
-// //     //candie is thinking
-//   setTimeout (() => {
-//     alert("Candie has made her choice. Click OK to continue.");
-//   }, 4000);
-//
-  playerWinsRound = (playerHand,computerHand) => {
+//event listeners
+const playersChoose =
+$(".choice").on('click', (e) => {
+  let playerHand = e.currentTarget.id;
+  let computerHand = rpsls[Math.floor(rpsls.length * Math.random())];
+  console.log(playerHand + ":" + computerHand);
+  // determineWhoWinsRound(playerHand,computerHand);
+  // playPlayerHand();
+  // playComputerHand();
+
+});
+
+//https://stackoverflow.com/questions/4944387/go-to-link-on-button-click-jquery
+// playerWinsRound
+const proclaimPlayerSuccess = () => {
     if (playerHand === "scissors" && computerHand === "paper"){
       playerWinsRound = true;
       playerScore++;
@@ -134,74 +90,55 @@ let gameTie = false; //no one has a tie game yet
     }
   }
 
+  //computer wins round
+const proclaimComputerSuccess = () => {
+  if (computerHand === scissors && playerHand === paper){
+    computerWinsRound = true;
+    computerScore++;
+  } else if (computerHand === paper && playerHand === rock) {
+    computerWinsRound = true;
+    computerScore++;
+  } else if (computerHand === rock && playerHand === lizard){
+    computerWinsRound = true;
+    computerScore++;
+  } else if (computerHand === lizard && playerHand === spock){
+    computerWinsRound = true;
+    computerScore++;
+  } else if (computerHand === spock && playerHand === scissors){
+    computerWinsRound = true;
+    computerScore++;
+  } else if (computerHand === scissors && playerHand === lizard){
+    computerWinsRound = true;
+    computerScore++;
+  } else if (computerHand === lizard && playerHand === paper){
+    computerWinsRound = true;
+    computerScore++;
+  } else if (computerHand === paper && playerHand === spock){
+    computerWinsRound = true;
+    computerScore++;
+  } else if (computerHand === spock && playerHand === rock){
+    computerWinsRound = true;
+    computerScore++;
+  } else if (computerHand === rock && playerHand === scissors){
+    computerWinsRound = true;
+    computerScore++;
+  }
+}
 
-//event handlers
-
-//event listeners
-$(".choice").on('click', (e) => {
-  let playerHand = e.currentTarget.id;
-  let computerHand = rpsls[Math.floor(rpsls.length * Math.random())];
-  console.log(playerHand + ":" + computerHand);
-  playerWinsRound(playerHand,computerHand);
-});
-
-
-
-
-//
-//
-//
-// //player wins round
-
-//
-// //================================
-//
-// //computer wins round
-// if (computerHand === scissors && playerHand === paper){
-//   computerWinsRound = true;
-//   computerScore++;
-// } else if (computerHand === paper && playerHand === rock) {
-//   computerWinsRound = true;
-//   computerScore++;
-// } else if (computerHand === rock && playerHand === lizard){
-//   computerWinsRound = true;
-//   computerScore++;
-// } else if (computerHand === lizard && playerHand === spock){
-//   computerWinsRound = true;
-//   computerScore++;
-// } else if (computerHand === spock && playerHand === scissors){
-//   computerWinsRound = true;
-//   computerScore++;
-// } else if (computerHand === scissors && playerHand === lizard){
-//   computerWinsRound = true;
-//   computerScore++;
-// } else if (computerHand === lizard && playerHand === paper){
-//   computerWinsRound = true;
-//   computerScore++;
-// } else if (computerHand === paper && playerHand === spock){
-//   computerWinsRound = true;
-//   computerScore++;
-// } else if (computerHand === spock && playerHand === rock){
-//   computerWinsRound = true;
-//   computerScore++;
-// } else if (computerHand === rock && playerHand === scissors){
-//   computerWinsRound = true;
-//   computerScore++;
-// }
-//
-// //=============================
-// //round tie
-// if (playerScore === rock && computerScore === rock){
-//   roundTie = true;
-// } else if (playerScore === paper && computerScore === paper){
-//   roundTie = true;
-// } else if (playerScore === scissors && computerScore === scissors){
-//   roundTie = true;
-// } else if (playerScore === lizard && computerScore === lizard){
-//   gameTie = true;
-// } else if (playerScore === spock && computerScore === spock){
-//   gameTie = true;
-// }
+  //round tie
+const declareTieRound = () => {
+  if (playerScore === rock && computerScore === rock){
+    roundTie = true;
+  } else if (playerScore === paper && computerScore === paper){
+    roundTie = true;
+  } else if (playerScore === scissors && computerScore === scissors){
+    roundTie = true;
+  } else if (playerScore === lizard && computerScore === lizard){
+    gameTie = true;
+  } else if (playerScore === spock && computerScore === spock){
+    gameTie = true;
+  }
+}
 //
 // //==============================
 //
@@ -227,33 +164,84 @@ $(".choice").on('click', (e) => {
 //
 // //====================================
 //
-// //game is tied under the following conditions
-// if (finalPlayerScore === 0 && finalComputerScore === 0){
-//   gameTie = true;
-// } else if (finalPlayerScore === 1 && finalComputerScore === 1){
-//   gameTie = true;
-// } else if (finalPlayerScore === 2 && finalComputerScore === 2){
-//   gameTie = true;
-// } else if (finalPlayerScore === 3 && finalComputerScore === 3){
-//   gameTie = true;
-// }
-//
+// //game is done under the following conditions - keep
+const determineWhoWinsGame = () => {
+  if (finalPlayerScore === finalComputerScore) {
+  gameTie = true;
+} else if (finalPlayerScore > finalComputerScore) {
+  playerWinsGame = true;
+} else if (finalComputerScore > finalPlayerScore) {
+  computerWinsGame = true;
+}
+}
+
+
+
 // //=====================================
-// //final destinations
+// //final destinations - keep
 //
-// if (playerWinsGame = true){
-//   //translate this to jQuery: onclick of final results button, go to
-//   window.location.href = "http://tkotan.github.io/html/final-player-wins.html";
+
+// const exitToFinalDestination = () => {
+//   if (playerWinsGame = true){
+//   $("#final-results").on('click', (e) => {
+//     document.location.href = "http://tkotan.github.io/html/final-player-wins.html";
 // } else if (computerWinsGame = true){
-//   //translate this to jQuery: onclick of final results button, go to
-//   window.location.href = "http://tkotan.github.io/html/final-computer-wins.html";
-// }(gameTie = true){
-//   //translate this to jQuery: onclick of final results button, go to
-//   window.location.href = "http://tkotan.github.io/html/final-tie-game.html";
+//   $("#final-results").on('click', (e) => {
+//     document.location.href = "http://tkotan.github.io/html/final-computer-wins.html";
+// } else if (gameTie = true){
+//   $("#final-results").on('click', (e) => {
+//     document.location.href = "http://tkotan.github.io/html/final-tie-game.html";
 // } else {
-//   //translate this to jQuery: onclick of final results button, go to
-//   window.location.href = "http://tkotan.github.io/index.html";
+//   $("#final-results").on('click', (e) => {
+//     document.location.href = "http://tkotan.github.io/index.html";
+//   )}
+//  )}
+//  )}
+//  )}
+//  }
 // }
+
+
+//grabbing the photo
+// const playPlayerHand = () => {
+// if (playerHand === "rock"){
+//   $('#rock-img');
+//
+// } else if (playerHand === "paper"){
+//   $('#paper-img').show();
+//
+// } else if (playerHand === "scissors") {
+//   $('#scissors-img').show();
+//
+// } else if (playerHand === "lizard") {
+//   $('#lizard-img').show();
+//
+// } else if (playerHand === "spock") {
+//   $('#spock-img').show();
+// }
+// }
+//
+//
+// const playComputerHand = () => {
+// if (computerHand === "rock"){
+//   $('#rock-img').show();
+//
+// } else if (computerHand === "paper"){
+//   $('#paper-img').show();
+//
+// } else if (computerHand === "scissors") {
+//   $('#scissors-img').show();
+//
+// } else if (computerHand === "lizard") {
+//   $('#lizards-img').show();
+//
+// } else if (computerHand === "spock") {
+//   $('#spock-img').show();
+// }
+// }
+
+
+
 reset();
 
 });
